aa142dee131b1ba370a929719bdd8dba501d7c03
AmirSTM <amirstm@gmail.com>
2023-08-21 21:15:16 +0330

    Seperated COPY command for requirements.txt and other python codes for not repeating pip install libs everytime the code changes.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.9.17
 
 WORKDIR /python-code
-COPY python/ .
+COPY python/requirements.txt .
 RUN pip install --no-cache-dir --upgrade pip && \
     pip install --no-cache-dir -r requirements.txt
+COPY python/ .
 
 CMD ["python", "test.py"]
--- python/test.py
+++ python/test.py
@@ -1,4 +1,11 @@
+import pandas as pd
 import numpy as np
+import jdatetime, datetime, json
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
-print(f"numpy is of version {np.__version__}")
-print("Hello world!")
+print(f"pandas version -> {pd.__version__}")
+print(f"numpy version -> {np.__version__}")
+print(f"matplotlib version -> {matplotlib.__version__}")
+print("Hello world 2!")
